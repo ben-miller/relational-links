bd25a39af9d0d0f6f13cff49fe8b135ebc65af61
Benjamin Miller <benmiller179@gmail.com>
2024-10-25 14:19:12 -0500

    Refactor

--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-import {MarkdownPostProcessorContext, Plugin, TAbstractFile, TFile} from "obsidian";
+import {Plugin, TAbstractFile, TFile} from "obsidian";
 import MarkdownIt from "markdown-it";
 import {getAllTokens, relationalLinksMarkdownPlugin} from "./lib/relationalLinksMarkdownPlugin";
 import {RelationalTagSuggestor} from "./lib/relationalTagSuggestor";
 import {RelationalLinkSuggestor} from "./lib/relationalLinkSuggestor";
+import {rlMarkdownPostProcessor} from "./lib/markdownPostProcessor";
 
 const md = MarkdownIt()
 md.use(relationalLinksMarkdownPlugin)
@@ -61,23 +62,7 @@ export default class RelationalLinksPlugin extends Plugin {
 	}
 
 	async initMarkdownPostProcessor() {
-		this.registerMarkdownPostProcessor((element: HTMLElement, context: MarkdownPostProcessorContext) => {
-			element.querySelectorAll("p").forEach((p) => {
-				p.innerHTML = p.innerHTML.replace(/#\[([a-zA-Z0-9._:-]+)\[(.*?)\]\]/g, (match, tag, linkPath) => {
-					const file = this.app.vault.getAbstractFileByPath(linkPath);
-					const tagLink = `<a href="#${tag}" class="tag" target="_blank" rel="noopener nofollow">${tag}</a>`;
-					let pathLink = "";
-					if (file && file instanceof TFile) {
-						const basename = file.basename;
-						pathLink = `<a data-ref="${basename}" href="${basename}" class="internal-link">${basename}</a>`;
-					} else {
-						const basename = linkPath.split('/').pop()?.replace(/\.[^/.]+$/, '') || linkPath;
-						pathLink = `<a data-ref="${basename}" href="${basename}" class="internal-link is-unresolved">${basename}</a>`;
-					}
-					return `#[${tagLink}[${pathLink}]]`;
-				});
-			});
-		});
+		this.registerMarkdownPostProcessor(rlMarkdownPostProcessor);
 	}
 
 	async onload() {
